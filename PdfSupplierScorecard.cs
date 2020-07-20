a27b9222977c19b133769c7193114d16ce791a96
Chris Li <chris.li@endataclaims.com>
2020-07-20 16:40:53 +1000

    update

---
+++ PdfSupplierScorecard.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+
+namespace PdfPlayGround
+{
+    using Model;
+    using System.Linq;
+
+    public class PdfSupplierScorecard: PdfBase
+    {
+        protected readonly ClaimJobReportForm Source;
+
+        public PdfSupplierScorecard(ClaimJobReportForm claimJob)
+        {
+            Source = claimJob;
+            PageMargin = new Margin(10, 10, 90, 20);
+            PageInfo = new Rectangle(PageSize.A4.Rotate());
+        }
+
+        protected override void WriteDocument()
+        {
+            base.WriteDocument();
+            Doc.AddAuthor("ENData");
+            Doc.AddCreator("ENData");
+            Doc.AddKeywords("This is an report of Supplier Scorecard");
+            Doc.AddSubject("Supplier Scorecard");
+            Doc.AddTitle(System.DateTime.Now.ToString("dd/MM/yyyy").Replace("-", " ") + "Report");
+
+            //Pdf Start 
+
+        }
+    }
+}
